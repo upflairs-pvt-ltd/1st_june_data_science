51feb273fe5f0b31751eb002cf57d56d1eabe0b2
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-03 20:31:18 +0530

    3rd june

---
+++ day2.py
@@ -0,0 +1,94 @@
+#int num = 10;  
+#integer --> whole number, complete number, without decimal point
+#python comments 
+
+
+# float numbers with decimal point 
+# var = 10.25
+# print(var)
+# print(type(var))
+
+# a-z , A-Z , 0-9 , _ ,
+# age = 21 
+# Age = 22 
+# AGE = 25 # variables are case sensitive
+# type = 25
+# print = 500
+
+# boolean 
+# quit = True
+# quit2 = False 
+# print(quit)
+# print(type(quit))
+
+# int , float , boolean , string 
+
+# st = "abc ABC 123 - _ + # @ $ %"
+# st2 = 'abc ABC 123 - _ + # @ $ %'
+# st3 = """
+# Hii we are upflairs,
+# and upflairs located in jaipur. """
+
+# print(st3)
+# print(type(st3))
+
+# [start:stop:step or jump]  # step poin --> 1 
+# company = "Upflairs"
+# print(company)
+# print(company[::-1]) # single character 
+# print(type(company))
+
+# U   p   f   l  a   i   r   s
+# 0   1   2   3  4   5   6   7   # positive indexing , forward indexing
+#-8  -7  -6  -5 -4  -3  -2  -1  # negative indexing , backward indexing
+
+
+# st = "upflairs pvt jaipur rajasthan"
+# print(st[13:19])  #jaipur 
+# print(len(st))  # it will return total no. of char. 
+# st2  = st.upper()
+# print(st2.lower())
+
+# print(st.count('ja'))
+# print(st.replace("fla",''))
+#mutable or imutable  
+
+#mutable --> changeble 
+#imutable --> unchangeble 
+# st[0] = "I"
+# print(st)
+
+
+# st = "upflairs pvt jaipur rajasthan"
+# count , len , upper , lower , replace 
+# print(st)
+# print(st.strip())  # left , right 
+# print(st.lstrip())
+# print(st.rstrip())
+# print(st.endswith("s"))
+# print(st.startswith('u'))
+# st = "A , C"
+# a , b = st.split(',')
+# print(a)
+# print(b)
+
+st = "upflairs"
+
+
+center ,
+st.ljust, 
+st.rjust
+st.islower
+st.isupper
+st.isdigit
+st.isalnum
+st.isalpha
+
+
+print(st.find("u"))
+
+
+
+
+
+
