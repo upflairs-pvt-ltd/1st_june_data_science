17ea5b125150ba3104ffa187e668f15b3f95b5ff
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-06-06 18:43:34 +0530

    6 june

---
+++ day4.py
@@ -0,0 +1,106 @@
+## <<<<<<<<< SET >>>>>>>>>>.
+# st = {41,41,52,41,63,96,45,85,25}
+# st = {1,1,2,2,3,3,3}
+# order maintain 
+# st.add(1000)
+# print(st)
+
+# st1 = {52,41,63,96,85,74,25}
+# st1.remove(960)  # error show 
+# st1.discard(960)   # error not show 
+# print(st1)
+
+
+# st1 = {52,41,63,96,85,74,25}
+# st2 = {52,41,63,75,65,64,96}
+
+# st1.update(st2)
+# print(st1)
+
+# print(st1.intersection(st2))
+
+# order maintain indexing 
+# st1 = {52,41,63,96,85,74,25}
+
+# tuple --> imutable  delete add 
+# set --> imutable delete and add 
+# st1[1] = 500  
+
+# print(st1[1])
+
+
+### <<<<<<<<<<<< DICTIONARY  >>>>>>>>>>>> 
+# pairs  ---> keys:value 
+# dt = {"A":10,"B":20,"C":30}
+# print(dt)
+# print(type(dt))
+# print(dt["A"])
+# dt['D'] = 40 
+# dt["B"] = 200
+
+# print(dt)
+
+
+# dt = {0:10,1:20,2:30}
+# print(dt[2])
+# print(type(dt))
+
+# student = {"Rahul":52,"Mohit":67,"Syam":78}
+# print(student["Mohit"])
+# print(student.keys())
+# print(student.values())
+
+# student = {"Name":["Rohit","Mohit","Nitin"],
+#             "Marks":[52,41,63],
+#             "Subject":"Math"}
+
+# student['Name'][1] = "Raju"
+# print(student)
+
+            # "Mohit" , 41 
+
+# Radhey , 78
+# student['Name'].insert(0,"Radhey")
+# student["Marks"].insert(0,78)
+# Math --> Science 
+# print(len(student["Name"]))
+
+
+## <<<<<<<<<<<<<, TYPE CASTING  >>>>>>>>>>>>>>
+
+# int, float , str , set ,list, dict, tuple 
+
+# var1 = 25  # integer 
+# ls = [1,2,3,4,5,6]
+# print(set(ls))
+# ls = [25,41,63,52,41,85,96,74,85,63,52,41,63,41]
+# print(list(set(ls)))
+
+
+# ls = [1,2,3,4,5,6,7,8,9,10]
+# ls = list(range(10,20))  #[0,1,2,3,4,5,19]  range(startin,ending, step)
+# print(ls)
+# even numbers 1 to 100 
+
+# even_numbers = list(range(2,101,2))
+# print(even_numbers)
+# print("Total even no. : ",len(even_numbers))
+
+# print("Hello world")
+# print()
+# print("Hello world")
+# print()
+# print("Hello world")
+
+print("Hello world 1",end=' upflairs ')  # '\n'
+print("Hello world 2",end=' upflairs ')
+print("Hello world 3",end=' upflairs ')
+# Hello world 1 Hello world 2 Hello world 3
+
+# Hello world 1
+
+
+
+
+ 
+
