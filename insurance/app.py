1a6edf14caa0e99697ade111ba9c8f1c36f1c405
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-18 17:28:34 +0530

    17july

--- insurance/app.py
+++ insurance/app.py
@@ -1,9 +1,30 @@
-from flask import Flask 
+from flask import Flask,render_template,url_for,request
 app = Flask(__name__)
 
 @app.route('/')  # url 
 def home():
-    return "Hii this is home page"
+    return render_template('home.html')
+
+
+@app.route('/project')  # http://127.0.0.1:5000/project
+def project():
+    return render_template('project.html')
+
+
+@app.route('/predict',methods=['GET','POST']) # http://127.0.0.1:5000/predict
+def predict():
+    if request.method == "POST":
+        # to recieve the data 
+        region = request.form['region']
+        children = request.form['children']
+        health = request.form['health']
+        smoker = request.form['smoker']
+        gender = request.form['gender']
+        bmi = request.form['bmi']
+        age = request.form['age']
+        unseen_data = [region,children,health,smoker,gender,bmi,age]
+        return unseen_data
+
 
 
 
