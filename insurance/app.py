5858c1a8d9c8a5561df155c53a3cb3e906a762f5
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-18 18:10:47 +0530

    dfd

--- insurance/app.py
+++ insurance/app.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template,url_for,request
+import joblib 
+random_forest = joblib.load('./models/randomforest.lb')  # loaded 
 app = Flask(__name__)
 
 @app.route('/')  # url 
@@ -16,17 +18,35 @@ def predict():
     if request.method == "POST":
         # to recieve the data 
         region = request.form['region']
-        children = request.form['children']
-        health = request.form['health']
-        smoker = request.form['smoker']
-        gender = request.form['gender']
-        bmi = request.form['bmi']
-        age = request.form['age']
-        unseen_data = [region,children,health,smoker,gender,bmi,age]
-        return unseen_data
+        children = int(request.form['children'])
+        health = int(request.form['health'])
+        smoker = int(request.form['smoker'])
+        gender = int(request.form['gender'])
+        bmi = int(request.form['bmi'])
+        age = int(request.form['age'])
+        region_northeast = 0
+        region_northwest = 0
+        region_southeast = 0
+        region_southwest = 0
+        if region == 'se':
+            region_southeast = 1 
+        elif region == 'sw':
+            region_southwest = 1
+        elif region == 'ne':
+            region_northeast = 1 
+        else:
+            region_northwest = 1
+
+        # x_variables 
+        unseen_data = [[age,gender,bmi,children,smoker,health,
+                        region_northeast,region_northwest,region_southeast,
+                        region_southwest]]
 
 
+        prediction = random_forest.predict(unseen_data)[0]
+        print(prediction)
 
+        return str(prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
