7e882cd4303ddc1f6bd2dc188a55a1c832005d1b
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-24 17:04:23 +0530

    project done

--- email_classification/app.py
+++ email_classification/app.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,url_for,request
 import joblib 
 
 bow_obj = joblib.load("./models/bag_of_words.lb")
+model = joblib.load("./models/bernouliNB.lb")   
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,13 +16,13 @@ def prediction():
         message = str(request.form['message'])
         email_message = [message]
         email_converted = bow_obj.transform(email_message).toarray()
+        prediction = model.predict(email_converted)[0]
+        label_dict = {'0':'Ham','1':'Spam'}
 
-        return message 
+        # return label_dict[str(prediction)] 
 
 
-
-
-
+        return render_template('output.html',output= label_dict[str(prediction)])
 if __name__ == "__main__":
     app.run(debug=True)
 
